c586241c854adabdc5486658cc52311e11744427
abhayayay <abhay.aggrawal@couchbase.com>
2022-12-19 15:52:09 +0530

    Android collection and scope dispatcher

---
+++ CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/CollectionRequestHandler.java
@@ -0,0 +1,18 @@
+package com.couchbase.mobiletestkit.javacommon.RequestHandler;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import com.couchbase.mobiletestkit.javacommon.Args;
+import com.couchbase.lite.Array;
+import com.couchbase.lite.Blob;
+import com.couchbase.lite.Dictionary;
+import com.couchbase.lite.Document;
+import com.couchbase.lite.DocumentChange;
+import com.couchbase.lite.DocumentChangeListener;
+import com.couchbase.lite.MutableDocument;
+
+public class CollectionRequestHandler {
+    
+}
