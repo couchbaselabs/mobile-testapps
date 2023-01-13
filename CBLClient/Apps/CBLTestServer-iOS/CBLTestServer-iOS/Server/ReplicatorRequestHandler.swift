dbf7dfa3e06366805b2738b78b7b299308eb05da
Abhay Aggrawal <108788393+abhayayay@users.noreply.github.com>
2023-01-13 17:13:44 +0530

    Update ReplicatorRequestHandler.swift

--- CBLClient/Apps/CBLTestServer-iOS/CBLTestServer-iOS/Server/ReplicatorRequestHandler.swift
+++ CBLClient/Apps/CBLTestServer-iOS/CBLTestServer-iOS/Server/ReplicatorRequestHandler.swift
@@ -25,7 +25,7 @@ public class ReplicatorRequestHandler {
             
         case "replicator_start":
             let replication_obj: Replicator = args.get(name: "replicator")!
-            try replication_obj.start()
+            replication_obj.start()
 
         case "replicator_stop":
             let replication_obj: Replicator = args.get(name: "replicator")!
