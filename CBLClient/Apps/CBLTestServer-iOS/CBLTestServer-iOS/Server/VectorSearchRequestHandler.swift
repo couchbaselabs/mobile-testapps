1f0b54c29067fe1483dec17cbe7fcd9983dc30f2
Monty Ali <monty.ali@couchbase.com>
2024-02-19 12:11:05 +0000

    Start work on VectorSearch handlers
    Add swift-transformers
    Add gte-small tokenizer configs
    First attempt at generating embeddings with gte-small model

---
+++ CBLClient/Apps/CBLTestServer-iOS/CBLTestServer-iOS/Server/VectorSearchRequestHandler.swift
@@ -0,0 +1,9 @@
+//
+//  VectorSearchRequestHandler.swift
+//  CBLTestServer-iOS
+//
+//  Created by Monty Ali on 13/02/2024.
+//  Copyright © 2024 Raghu Sarangapani. All rights reserved.
+//
+
+import Foundation
