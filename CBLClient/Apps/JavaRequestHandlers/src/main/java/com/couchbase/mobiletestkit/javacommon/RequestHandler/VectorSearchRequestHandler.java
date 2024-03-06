76538b920a08b3b46744f0a5ff70b3eb52cfb4ad
Mike <mike.stanton@couchbase.com>
2024-03-06 16:55:21 +0000

    building out VectorSearchRequestHandler.java for Android test app

--- CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/VectorSearchRequestHandler.java
+++ CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/VectorSearchRequestHandler.java
@@ -2,7 +2,6 @@ package com.couchbase.mobiletestkit.javacommon.RequestHandler;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Map;
 import java.util.HashMap;
 
