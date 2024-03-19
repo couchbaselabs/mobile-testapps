c367e59068972a93069649adfdaa654d84b55912
Gilad Kalchheim <gilkal1979@gmail.com>
2024-03-19 09:08:06 +0000

    Change the class to use methods

--- CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/VectorSearchRequestHandler.java
+++ CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/VectorSearchRequestHandler.java
@@ -124,6 +124,102 @@ public class VectorSearchRequestHandler {
                 throw new Exception(method);
         }
     }*/
+    public String createIndex(Args args) {
+        Database database = args.get("database");
+
+        String scopeName = args.get("scopeName") != null ? args.get("scopeName") : "_default";
+        String collectionName = args.get("collectionName") != null ? args.get("collectionName") : "_default";
+
+        Collection collection = database.getCollection(collectionName, scopeName);
+        if (collection == null) {
+            throw new Exception("Could not open specified collection");
+        }
+
+        String indexName = args.get("indexName");
+
+        String expression = args.get("expression");
+
+        int dimensions = args.get("dimensions");
+        int centroids = args.get("centroids");
+
+        VectorEncoding.ScalarQuantizerType scalarEncoding = args.get("scalarEncoding");
+
+        Integer subquantizers = args.get("subquantizers");
+        Integer bits = args.get("bits");
+
+        String metric = args.get("metric");
+
+        Integer minTrainingSize = args.get("minTrainingSize");
+        Integer maxTrainingSize = args.get("maxTrainingSize");
+
+        if (scalarEncoding != null && (bits != null || subquantizers != null)) {
+            throw new Exception(
+                            "Cannot have scalar quantization and arguments for product quantization at the same time");
+        }
+
+        if ((bits != null && subquantizers == null) || (bits == null && subquantizers != null)) {
+            throw new Exception("Product quantization requires both bits and subquantizers set");
+        }
+
+        VectorIndexConfiguration config = new VectorIndexConfiguration(expression, dimensions, centroids);
+        if (scalarEncoding != null) {
+            config.setEncoding(VectorEncoding.scalarQuantizer(scalarEncoding));
+        }
+        if (bits != null) {
+            config.setEncoding(VectorEncoding.productQuantizer(subquantizers, bits));
+        }
+        if (metric != null) {
+            switch (metric) {
+                case "euclidean":
+                    config.setMetric(VectorIndexConfiguration.DistanceMetric.EUCLIDIAN);
+                    break;
+                 case "cosine":
+                    config.setMetric(VectorIndexConfiguration.DistanceMetric.COSINE);
+                    break;
+                default:
+                    throw new Error("Invalid distance metric");
+                }
+            }
+
+            if (minTrainingSize != null) {
+                config.setMinTrainingSize(minTrainingSize);
+            }
+
+            if (maxTrainingSize != null) {
+                config.setMaxTrainingSize(maxTrainingSize);
+            }
+
+            collection.createIndex(indexName, config);
+
+            return "???";
+        }
+    
+    public List<Object> query(Args args) {
+        String term = args.get("term");
+
+        Args embeddingArgs = new Args();
+        embeddingArgs.put(term, "input");
+        Object embeddedTerm = this.getEmbedding(embeddingArgs);
+
+        String sql = args.get("sql");
+
+        Database db = args.get("database");
+
+        Parameters params = new Parameters();
+        params.setValue("vector", embeddedTerm);
+        Query query = db.createQuery(sql);
+        query.setParameters(params);
+
+        List<Object> resultArray = new ArrayList<>();
+        ResultSet queryResults = query.execute();
+        for (Result row : queryResults) {
+            resultArray.add(row.toMap());
+        }
+
+        return resultArray;
+    }
+
+
     public Dictionary getEmbedding(Args args) throws CouchbaseLiteException{
         Database db3 = this.loadDatabase(args);
         vectorModel model1 = new vectorModel("test", db3);
