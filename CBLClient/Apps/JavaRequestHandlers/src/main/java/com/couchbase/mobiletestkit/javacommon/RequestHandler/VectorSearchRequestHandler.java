8345ff5f96d4ad4095562448d4c7ef90109ffb5f
Gilad Kalchheim <gilkal1979@gmail.com>
2024-03-19 12:37:10 +0000

    Change the database path input

--- CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/VectorSearchRequestHandler.java
+++ CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/VectorSearchRequestHandler.java
@@ -235,7 +235,7 @@ public class VectorSearchRequestHandler {
         // loads the given database vsTestDatabase
         DatabaseRequestHandler dbHandler = new DatabaseRequestHandler();
         Args newArgs = args;
-        newArgs.put("dbPath", "assets/vsTestDatabase.cblite2");
+        newArgs.put("dbPath", "vstestDatabase.cblite2.zip");
         String dbPath = dbHandler.getPreBuiltDb(newArgs);
         newArgs.put("dbPath", dbPath);
         newArgs.put("dbName", "vsTestDatabase");
