9546eea187986912b2b432593f4efb56426f4c2c
Mike <mike.stanton@couchbase.com>
2024-03-21 10:14:36 +0000

    fix typo

--- CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/VectorSearchRequestHandler.java
+++ CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/VectorSearchRequestHandler.java
@@ -188,7 +188,7 @@ public class VectorSearchRequestHandler {
         if (metric != null) {
             switch (metric) {
                 case "euclidean":
-                    config.setMetric(VectorIndexConfiguration.DistanceMetric.EUCLIDEAN);
+                    config.setMetric(VectorIndexConfiguration.DistanceMetric.EUCLIDIAN);
                     break;
                 case "cosine":
                     config.setMetric(VectorIndexConfiguration.DistanceMetric.COSINE);
