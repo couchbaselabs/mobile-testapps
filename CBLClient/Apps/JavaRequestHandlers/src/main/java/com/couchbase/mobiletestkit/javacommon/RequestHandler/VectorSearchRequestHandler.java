b9cd645cce328488107799439c84554b20dda3bc
Mike <mike.stanton@couchbase.com>
2024-04-16 11:14:47 +0100

    refactor predict function to avoid timeout

--- CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/VectorSearchRequestHandler.java
+++ CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandler/VectorSearchRequestHandler.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -14,133 +16,37 @@ import com.couchbase.mobiletestkit.javacommon.util.Log;
 
 public class VectorSearchRequestHandler {
     private static final String TAG = "GILAD";
+    private final Map<String, Object> wordMap = getWordVectMap("giladDB");
 
-    /*
-     * public Object handleRequest(String method, Args args) throws Exception {
-     * switch (method) {
-     * case "createIndex":
-     * Database database = args.get("database");
-     * 
-     * String scopeName = args.get("scopeName") != null ? args.get("scopeName") :
-     * "_default";
-     * String collectionName = args.get("collectionName") != null ?
-     * args.get("collectionName") : "_default";
-     * 
-     * Collection collection = database.getCollection(collectionName, scopeName);
-     * if (collection == null) {
-     * throw new Exception("Could not open specified collection");
-     * }
-     * 
-     * String indexName = args.get("indexName");
-     * 
-     * String expression = args.get("expression");
-     * 
-     * int dimensions = args.get("dimensions");
-     * int centroids = args.get("centroids");
-     * 
-     * VectorEncoding.ScalarQuantizerType scalarEncoding =
-     * args.get("scalarEncoding");
-     * 
-     * Integer subquantizers = args.get("subquantizers");
-     * Integer bits = args.get("bits");
-     * 
-     * String metric = args.get("metric");
-     * 
-     * Integer minTrainingSize = args.get("minTrainingSize");
-     * Integer maxTrainingSize = args.get("maxTrainingSize");
-     * 
-     * if (scalarEncoding != null && (bits != null || subquantizers != null)) {
-     * throw new Exception(
-     * "Cannot have scalar quantization and arguments for product quantization at the same time"
-     * );
-     * }
-     * 
-     * if ((bits != null && subquantizers == null) || (bits == null && subquantizers
-     * != null)) {
-     * throw new
-     * Exception("Product quantization requires both bits and subquantizers set");
-     * }
-     * 
-     * VectorIndexConfiguration config = new VectorIndexConfiguration(expression,
-     * dimensions, centroids);
-     * if (scalarEncoding != null) {
-     * config.setEncoding(VectorEncoding.scalarQuantizer(scalarEncoding));
-     * }
-     * if (bits != null) {
-     * config.setEncoding(VectorEncoding.productQuantizer(subquantizers, bits));
-     * }
-     * if (metric != null) {
-     * switch (metric) {
-     * case "euclidean":
-     * config.setMetric(VectorIndexConfiguration.DistanceMetric.EUCLIDIAN);
-     * break;
-     * case "cosine":
-     * config.setMetric(VectorIndexConfiguration.DistanceMetric.COSINE);
-     * break;
-     * default:
-     * throw new Error("Invalid distance metric");
-     * }
-     * }
-     * 
-     * if (minTrainingSize != null) {
-     * config.setMinTrainingSize(minTrainingSize);
-     * }
-     * 
-     * if (maxTrainingSize != null) {
-     * config.setMaxTrainingSize(maxTrainingSize);
-     * }
-     * 
-     * collection.createIndex(indexName, config);
-     * 
-     * return "???";
-     * 
-     * case "registerModel":
-     * String key = args.get("key");
-     * String name = args.get("name");
-     * vectorModel model = new vectorModel(key);
-     * Database.prediction.registerModel(name, model);
-     * return "Registered model with name " + name;
-     * 
-     * case "query":
-     * String term = args.get("term");
-     * 
-     * Args embeddingArgs = new Args();
-     * embeddingArgs.put(term, "input");
-     * Object embeddedTerm = this.handleRequest("vectorSearch_getEmbedding",
-     * embeddingArgs);
-     * 
-     * String sql = args.get("sql");
-     * 
-     * Database db = args.get("database");
-     * 
-     * Parameters params = new Parameters();
-     * params.setValue("vector", embeddedTerm);
-     * Query query = db.createQuery(sql);
-     * query.setParameters(params);
-     * 
-     * List<Object> resultArray = new ArrayList<>();
-     * ResultSet queryResults = query.execute();
-     * for (Result row : queryResults) {
-     * resultArray.add(row.toMap());
-     * }
-     * 
-     * return resultArray;
-     * 
-     * case "getEmbedding":
-     * Database db3 = (Database) handleRequest("vectorSearch_loadDatabase", args);
-     * vectorModel model1 = new vectorModel("test", db3);
-     * MutableDictionary testDic = new MutableDictionary();
-     * String input = args.get("input");
-     * testDic.setValue(input, "test");
-     * Dictionary value = model1.predict(testDic);
-     * 
-     * return value;
-     * 
-     * default:
-     * throw new Exception(method);
-     * }
-     * }
-     */
+    Map<String, Object> getWordVectMap(String dbName) {
+        Database db = new Database(dbName);
+        String sql1 = String.format("select word, vector from docBodyVectors");
+        Query query1 = db.createQuery(sql1);
+        ResultSet rs1 = query1.execute();
+        String sql2 = String.format("select word, vector from searchTerms");
+        Query query2 = db.createQuery(sql2);
+        ResultSet rs2 = query2.execute();
+
+        Map<String, Object> wordMap = new HashMap<String, Object>();
+
+        try {
+            List<Result> rl = rs1.allResults();
+            List<Result> rl2 = rs2.allResults();
+            rl.addAll(rl2);
+
+            for (Result r : rl) {
+                String word = r.getString("word");
+                Object vector = r.getDouble("vector");
+                wordMap.put(word, vector);
+            }
+            return wordMap;
+
+        } catch (Exception e) {
+            System.err.println(e + "retrieving vector could not be done - getWordVector query returned no results");
+            return null;
+        }
+    }
+
     public String createIndex(Args args) throws CouchbaseLiteException, Exception {
         Database database = args.get("database");
         Log.d(TAG, "*******CHECKING THAT RUNNING THE LATEST BUILD*********");
@@ -293,38 +199,34 @@ public class VectorSearchRequestHandler {
                 System.err.println(e + "Error creating instance of vectorModel");
             }
         }
-
-        Object getWordVector(String word) throws CouchbaseLiteException {
-            Database db = new Database(this.dbName);
-            String sql = String.format("select vector from searchTerms where word = '%s'", word);
-            Query query = db.createQuery(sql);
-            ResultSet rs = query.execute();
-            db.close();
-            try {
-                List<Result> rl = rs.allResults();
-                Map<String, Object> res = rl.get(0).toMap();
-                Log.d("getWordVector", "vector=" + res);
-                Log.d("getWordVector", "vector=" + res.get("vector"));
-                return res.get("vector");
-            } catch (Exception e) {
-                System.err.println(e + "retrieving vector could not be done - getWordVector query returned no results");
-                return null;
-            }
-        }
+        /*
+         * Object getWordVector(String word) throws CouchbaseLiteException {
+         * Database db = new Database(this.dbName);
+         * String sql =
+         * String.format("select vector from searchTerms where word = '%s'", word);
+         * Query query = db.createQuery(sql);
+         * ResultSet rs = query.execute();
+         * db.close();
+         * try {
+         * List<Result> rl = rs.allResults();
+         * Map<String, Object> res = rl.get(0).toMap();
+         * Log.d("getWordVector", "vector=" + res);
+         * Log.d("getWordVector", "vector=" + res.get("vector"));
+         * return res.get("vector");
+         * } catch (Exception e) {
+         * System.err.println(e +
+         * "retrieving vector could not be done - getWordVector query returned no results"
+         * );
+         * return null;
+         * }
+         * }
+         */
 
         @Override
         public Dictionary predict(Dictionary input) {
             String inputWord = input.getString(this.key);
-
             Object result = new ArrayList<>();
-
-            try {
-                result = getWordVector(inputWord);
-            } catch (CouchbaseLiteException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-
+            result = wordMap.get(inputWord);
             MutableDictionary output = new MutableDictionary();
             output.setValue("vector", result);
             return output;
