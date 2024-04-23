387afef609c6c1e1b8fa37ad7e44c8a69d32a5ce
Mike <mike.stanton@couchbase.com>
2024-04-23 11:44:06 +0100

    built out createIndex method

--- CBLClient/Apps/CBLTestServer-Dotnet/TestServer/VectorSearchMethods.cs
+++ CBLClient/Apps/CBLTestServer-Dotnet/TestServer/VectorSearchMethods.cs
@@ -19,11 +19,89 @@ namespace Couchbase.Lite.Testing
                                        [NotNull] IReadOnlyDictionary<string, object> postBody,
                                        [NotNull] HttpListenerResponse response)
         {
+            // using db get defualt collection and store in Collection collection
             With<Database>(postBody, "database", database =>
             {
-                response.WriteBody(MemoryMap.Store(database.GetDefaultCollection()));
+                Collection collection = database.GetDefaultCollection();
             });
+
+            if (collection == null)
+            {
+                throw new InvalidOperationException("Could not open specified collection");
+            }
+
+            // get values from postBody
+            string indexName = postBody["indexName"].ToString();
+            string expression = postBody["expression"].ToString();
+
+            IExpression dimensions = expression.Int((int)postBody["dimensions"]);
+            IExpression centroids = expression.Int((int)postBody["centroids"]);
+
+            VectorEncoding.ScalarQuantizerType scalarEncoding = postBody["scalarEncoding"]; // unsure about the types and get method here
+
+            IExpression subquantizers = expression.Int((int)postBody["subquantizers"]);
+            IExpression bits = expression.Int((int)postBody["bits"]);
+
+            string metric = postBody["metric"].ToString();
+
+            IExpression minTrainingSize = expression.Int((int)postBody["minTrainingSize"]);
+            IExpression maxTrainingSize = expression.Int((int)postBody["maxTrainingSize"]);
+
+            // correctness checks
+            if (scalarEncoding != null && (bits != null || subquantizers != null))
+            {
+                throw new InvalidOperationException("Cannot have scalar quantization and arguments for product quantization at the same time");
+            }
+
+            if ((bits != null && subquantizers == null) || (bits == null && subquantizers != null))
+            {
+                throw new InvalidOperationException("Product quantization requires both bits and subquantizers set");
+            }
+
+            VectorIndexConfiguration config = new VectorIndexConfiguration(expression, dimensions, centroids); // unure on types here again, undocumented specifics
+
+            if (scalarEncoding != null)
+            {
+                config.SetEncoding(VectorEncoding.ScalarQuantizer(scalarEncoding));
+            }
+            if (bits != null)
+            {
+                config.SetEncoding(VectorEncoding.ProductQuantizer(subquantizers, bits));
+            }
+            if (metric != null)
+            {
+                switch (metric)
+                {
+                    case "euclidean":
+                        config.SetMetric(VectorIndexConfiguration.DistanceMetric.EUCLIDEAN);
+                        break;
+                    case "cosine":
+                        config.SetMetric(VectorIndexConfiguration.DistanceMetric.COSINE);
+                        break;
+                    default:
+                        throw new Exception("Invalid distance metric");
+                }
+            }
+
+            if (minTrainingSize != null)
+            {
+                config.SetMinTrainingSize(minTrainingSize);
+            }
+            if (maxTrainingSize != null)
+            {
+                config.MaxTrainingSize(maxTrainingSize);
+            }
+
+            try
+            {
+                collection.CreateIndex(indexName, config);
+                response.WriteBody("Created index with name " + indexName);
+            }
+            catch (Exception e)
+            {
+                response.WriteBody("Could not create index: " + e);
+            }
         }
 
     }
-}   
+}
