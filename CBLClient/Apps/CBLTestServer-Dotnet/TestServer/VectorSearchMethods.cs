5c0ff6423e83174240c3208d7300f396de484e86
Mike <mike.stanton@couchbase.com>
2024-05-09 12:53:03 +0100

    fixed syntax

--- CBLClient/Apps/CBLTestServer-Dotnet/TestServer/VectorSearchMethods.cs
+++ CBLClient/Apps/CBLTestServer-Dotnet/TestServer/VectorSearchMethods.cs
@@ -28,40 +28,40 @@ namespace Couchbase.Lite.Testing
             With<Database>(postBody, "database", database =>
             {
                 Collection collection = database.GetDefaultCollection() ?? throw new InvalidOperationException("Could not open specified collection");
-                Console.WriteLine("=========================================AT START")
+                Console.WriteLine("=========================================AT START");
                 // get values from postBody
                 string indexName = postBody["indexName"].ToString();
-                Console.WriteLine("=========================================After indexName")
+                Console.WriteLine("=========================================After indexName");
                 string expression = postBody["expression"].ToString();
-                Console.WriteLine("=========================================After expression")
+                Console.WriteLine("=========================================After expression");
 
                 // null coalescing checks
                 var dimensions = postBody["dimensions"];
-                Console.WriteLine("=========================================After dimensions")
-               // Console.WriteLine("================ dimensions = " + dimensions);
+                Console.WriteLine("=========================================After dimensions");
+                // Console.WriteLine("================ dimensions = " + dimensions);
 
                 var centroids = postBody["centroids"];
                 //Console.WriteLine("================ centroids = " + centroids);
-                Console.WriteLine("=========================================After centroids")
+                Console.WriteLine("=========================================After centroids");
 
                 var minTrainingSize = postBody["minTrainingSize"];
                 //Console.WriteLine("================ minTrainingSize = " + minTrainingSize);
-                Console.WriteLine("=========================================After minTrainingSize")
+                Console.WriteLine("=========================================After minTrainingSize");
 
                 var maxTrainingSize = postBody["maxTrainingSize"];
-                Console.WriteLine("=========================================After maxTrainingSize")
+                Console.WriteLine("=========================================After maxTrainingSize");
                 //Console.WriteLine("================ maxTrainingSize = " + maxTrainingSize);
 
-                var bits = 0;
-                var subquantizers = 0;
+                uint? bits = 0;
+                uint? subquantizers = 0;
                 ScalarQuantizerType? scalarEncoding = new();
 
                 try
                 {
-                    bits = postBody["bits"];
-                    Console.WriteLine("=========================================After bits")
-                    subquantizers = postBody["subquantizers"];
-                    Console.WriteLine("=========================================After subquantizers")
+                    bits = (uint)postBody["bits"];
+                    Console.WriteLine("=========================================After bits");
+                    subquantizers = (uint)postBody["subquantizers"];
+                    Console.WriteLine("=========================================After subquantizers");
                     //Console.WriteLine("------ bits + subs assigned");
                 }
                 catch (Exception e)
@@ -75,13 +75,13 @@ namespace Couchbase.Lite.Testing
                 try
                 {
                     scalarEncoding = (ScalarQuantizerType)postBody["scalarEncoding"];
-                    Console.WriteLine("=========================================After scalarEncoding")
-                //    Console.WriteLine("------ scalarEncoding assigned");
+                    Console.WriteLine("=========================================After scalarEncoding");
+                    //    Console.WriteLine("------ scalarEncoding assigned");
                 }
                 catch (Exception e)
                 {
                     scalarEncoding = null;
-                    Console.WriteLine("=========================================At scalarEncoding=NULL")
+                    Console.WriteLine("=========================================At scalarEncoding=NULL");
                 }
                 //Console.WriteLine("================ scalarEncoding = " + scalarEncoding);
 
@@ -90,7 +90,7 @@ namespace Couchbase.Lite.Testing
 
                 string metric = postBody["metric"].ToString();
                 //Console.WriteLine("================ metric = " + metric);
-                Console.WriteLine("=========================================After metric")
+                Console.WriteLine("=========================================After metric");
 
                 // correctness checks
                 if (scalarEncoding != null && (bits != null || subquantizers != null))
@@ -104,7 +104,7 @@ namespace Couchbase.Lite.Testing
                 }
 
                 // setting values based on config from input
-                Console.WriteLine("=========================================Before VectorEncoding encoding")
+                Console.WriteLine("=========================================Before VectorEncoding encoding");
                 VectorEncoding encoding = null;
                 if (scalarEncoding != null)
                 {
@@ -114,7 +114,7 @@ namespace Couchbase.Lite.Testing
                 {
                     encoding = VectorEncoding.ProductQuantizer((uint)subquantizers, (uint)bits);
                 }
-                Console.WriteLine("=========================================Before  DistanceMetric dMetric ")
+                Console.WriteLine("=========================================Before  DistanceMetric dMetric ");
                 DistanceMetric dMetric = new();
                 if (metric != null)
                 {
@@ -130,7 +130,7 @@ namespace Couchbase.Lite.Testing
                             throw new Exception("Invalid distance metric");
                     }
                 }
-                Console.WriteLine("=========================================Before  DVectorIndexConfiguration config")
+                Console.WriteLine("=========================================Before  DVectorIndexConfiguration config");
                 VectorIndexConfiguration config = new(expression, (uint)dimensions, (uint)centroids) // unure on types here again, undocumented specifics
                 {
                     Encoding = encoding,
@@ -139,7 +139,7 @@ namespace Couchbase.Lite.Testing
                     MaxTrainingSize = (uint)maxTrainingSize
                 };
 
-                Console.WriteLine("=========================================Before  creating final index")
+                Console.WriteLine("=========================================Before  creating final index");
                 try
                 {
                     collection.CreateIndex(indexName, config);
