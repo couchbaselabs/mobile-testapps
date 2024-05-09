46ade8b7163a42e7c8b26e01a4079da10724769a
Mike <mike.stanton@couchbase.com>
2024-05-09 14:40:27 +0100

    hardcoded values for create index

--- CBLClient/Apps/CBLTestServer-Dotnet/TestServer/VectorSearchMethods.cs
+++ CBLClient/Apps/CBLTestServer-Dotnet/TestServer/VectorSearchMethods.cs
@@ -36,107 +36,123 @@ namespace Couchbase.Lite.Testing
                 Console.WriteLine("=========================================After expression");
 
                 // null coalescing checks
-                uint dimensions = (uint)postBody["dimensions"];
-                Console.WriteLine("=========================================After dimensions");
+                // uint dimensions = (uint)postBody["dimensions"];
+                // Console.WriteLine("=========================================After dimensions");
 
-                uint centroids = (uint)postBody["centroids"];
-                Console.WriteLine("=========================================After centroids");
+                // uint centroids = (uint)postBody["centroids"];
+                // Console.WriteLine("=========================================After centroids");
 
-                uint minTrainingSize = (uint)postBody["minTrainingSize"];
-                Console.WriteLine("=========================================After minTrainingSize");
+                // uint minTrainingSize = (uint)postBody["minTrainingSize"];
+                // Console.WriteLine("=========================================After minTrainingSize");
 
-                uint maxTrainingSize = (uint)postBody["maxTrainingSize"];
-                Console.WriteLine("=========================================After maxTrainingSize");
+                // uint maxTrainingSize = (uint)postBody["maxTrainingSize"];
+                // Console.WriteLine("=========================================After maxTrainingSize");
 
-                uint? bits = 0;
-                uint? subquantizers = 0;
-                ScalarQuantizerType? scalarEncoding = new();
+                // uint? bits = 0;
+                // uint? subquantizers = 0;
+                // ScalarQuantizerType? scalarEncoding = new();
 
-                try
+                // try
+                // {
+                //     bits = (uint)postBody["bits"];
+                //     Console.WriteLine("=========================================After bits");
+                //     subquantizers = (uint)postBody["subquantizers"];
+                //     Console.WriteLine("=========================================After subquantizers");
+                // }
+                // catch (Exception e)
+                // {
+                //     bits = null;
+                //     subquantizers = null;
+                // }
+
+                // try
+                // {
+                //     scalarEncoding = (ScalarQuantizerType)postBody["scalarEncoding"];
+                //     Console.WriteLine("=========================================After scalarEncoding");
+                // }
+                // catch (Exception e)
+                // {
+                //     scalarEncoding = null;
+                //     Console.WriteLine("=========================================At scalarEncoding=NULL");
+                // }
+
+
+
+
+                // string metric = postBody["metric"].ToString();
+                // Console.WriteLine("=========================================After metric");
+
+                // // correctness checks
+                // if (scalarEncoding != null && (bits != null || subquantizers != null))
+                // {
+                //     throw new InvalidOperationException("Cannot have scalar quantization and arguments for product quantization at the same time");
+                // }
+
+                // if ((bits != null && subquantizers == null) || (bits == null && subquantizers != null))
+                // {
+                //     throw new InvalidOperationException("Product quantization requires both bits and subquantizers set");
+                // }
+                // if (bits == null && subquantizers == null)
+                // {
+                //     Console.WriteLine("======== broken because bits and subquantizers are null");
+                //     throw new InvalidOperationException("broken");
+                // }
+
+                // // setting values based on config from input
+                // Console.WriteLine("=========================================Before VectorEncoding encoding");
+                // VectorEncoding encoding = VectorEncoding.None();
+                // if (scalarEncoding != null)
+                // {
+                //     encoding = VectorEncoding.ScalarQuantizer((ScalarQuantizerType)scalarEncoding);
+                // }
+                // if (bits != null)
+                // {
+                //     encoding = VectorEncoding.ProductQuantizer((uint)subquantizers, (uint)bits);
+                // }
+                // Console.WriteLine("=========================================Before  DistanceMetric dMetric ");
+                // DistanceMetric dMetric = new();
+                // if (metric != null)
+                // {
+                //     switch (metric)
+                //     {
+                //         case "euclidean":
+                //             dMetric = DistanceMetric.Euclidean;
+                //             break;
+                //         case "cosine":
+                //             dMetric = DistanceMetric.Cosine;
+                //             break;
+                //         default:
+                //             throw new Exception("Invalid distance metric");
+                //     }
+                // }
+
+                // Console.WriteLine("========== DEBUG PRINTING VALUES ===============");
+                // Console.WriteLine("expression == " + expression + ", dimensions == " + dimensions + ", centroids == " + centroids + ", encoding == " + encoding + ", distance metric == " + dMetric + ", minSize == " + minTrainingSize + ", maxSize == " + maxTrainingSize);
+
+                // Console.WriteLine("=========================================Before  DVectorIndexConfiguration config");
+
+                const uint xDIMENSIONS = 300;
+                const uint xCENTROIDS = 20;
+                const uint xMIN_TRAINING_SIZE = 100;
+                const uint xMAX_TRAINING_SIZE = 200;
+                const string xEXPRESSION = "vector";
+                const DistanceMetric xMETRIC = DistanceMetric.Cosine;
+
+                var config = new VectorIndexConfiguration(xEXPRESSION, xDIMENSIONS, xCENTROIDS)
                 {
-                    bits = (uint)postBody["bits"];
-                    Console.WriteLine("=========================================After bits");
-                    subquantizers = (uint)postBody["subquantizers"];
-                    Console.WriteLine("=========================================After subquantizers");
-                }
-                catch (Exception e)
-                {
-                    bits = null;
-                    subquantizers = null;
-                }
-
-                try
-                {
-                    scalarEncoding = (ScalarQuantizerType)postBody["scalarEncoding"];
-                    Console.WriteLine("=========================================After scalarEncoding");
-                }
-                catch (Exception e)
-                {
-                    scalarEncoding = null;
-                    Console.WriteLine("=========================================At scalarEncoding=NULL");
-                }
-
-
-
-
-                string metric = postBody["metric"].ToString();
-                Console.WriteLine("=========================================After metric");
-
-                // correctness checks
-                if (scalarEncoding != null && (bits != null || subquantizers != null))
-                {
-                    throw new InvalidOperationException("Cannot have scalar quantization and arguments for product quantization at the same time");
-                }
-
-                if ((bits != null && subquantizers == null) || (bits == null && subquantizers != null))
-                {
-                    throw new InvalidOperationException("Product quantization requires both bits and subquantizers set");
-                }
-                if (bits == null && subquantizers == null)
-                {
-                    Console.WriteLine("======== broken because bits and subquantizers are null");
-                    throw new InvalidOperationException("broken");
-                }
-
-                // setting values based on config from input
-                Console.WriteLine("=========================================Before VectorEncoding encoding");
-                VectorEncoding encoding = VectorEncoding.None();
-                if (scalarEncoding != null)
-                {
-                    encoding = VectorEncoding.ScalarQuantizer((ScalarQuantizerType)scalarEncoding);
-                }
-                if (bits != null)
-                {
-                    encoding = VectorEncoding.ProductQuantizer((uint)subquantizers, (uint)bits);
-                }
-                Console.WriteLine("=========================================Before  DistanceMetric dMetric ");
-                DistanceMetric dMetric = new();
-                if (metric != null)
-                {
-                    switch (metric)
-                    {
-                        case "euclidean":
-                            dMetric = DistanceMetric.Euclidean;
-                            break;
-                        case "cosine":
-                            dMetric = DistanceMetric.Cosine;
-                            break;
-                        default:
-                            throw new Exception("Invalid distance metric");
-                    }
-                }
-
-                Console.WriteLine("========== DEBUG PRINTING VALUES ===============");
-                Console.WriteLine("expression == " + expression + ", dimensions == " + dimensions + ", centroids == " + centroids + ", encoding == " + encoding + ", distance metric == " + dMetric + ", minSize == " + minTrainingSize + ", maxSize == " + maxTrainingSize);
-
-                Console.WriteLine("=========================================Before  DVectorIndexConfiguration config");
-                VectorIndexConfiguration config = new(expression, dimensions, centroids) // unure on types here again, undocumented specifics
-                {
-                    Encoding = encoding,
-                    DistanceMetric = dMetric,
-                    MinTrainingSize = minTrainingSize,
-                    MaxTrainingSize = maxTrainingSize
+                    Encoding = VectorEncoding.None(),
+                    DistanceMetric = xMETRIC,
+                    MinTrainingSize = xMIN_TRAINING_SIZE,
+                    MaxTrainingSize = xMAX_TRAINING_SIZE
                 };
+
+                // VectorIndexConfiguration config = new(expression, dimensions, centroids) // unure on types here again, undocumented specifics
+                // {
+                //     Encoding = encoding,
+                //     DistanceMetric = dMetric,
+                //     MinTrainingSize = minTrainingSize,
+                //     MaxTrainingSize = maxTrainingSize
+                // };
 
                 Console.WriteLine("=========================================Before  creating final index");
                 try
