fbf0b9ef377e19c70fc52d8a9773f74eacf053ae
Mike <mike.stanton@couchbase.com>
2024-05-28 09:37:28 +0100

    syntax changes

--- CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
+++ CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
@@ -17,77 +17,78 @@ namespace vectorSearch_methods
 
     void vectorSearch_createIndex(json& body, mg_connection* conn)
     {
+        auto scopeName = body["scopeName"].get<string>();
+        auto collectionName = body["collectionName"].get<string>();
+        auto indexName = body["indexName"].get<string>();
+        auto expression = body["expression"].get<string>();
+        auto metric = body["metric"].get<string>();
+
+        auto dimensions = body["dimensions"].get<uint32_t>();
+        auto centroids = body["centroids"].get<uint32_t>();
+        auto minTrainingSize = body["minTrainingSize"].get<uint32_t>();
+        auto maxTrainingSize = body["maxTrainingSize"].get<uint32_t>();
+
+        std::optional<uint32_t> bits;
+        std::optional<uint32_t> subquantizers;
+        std::optional<CBLScalarQuantizerType> scalarEncoding;
+        std::optional<CBLDistanceMetric> dMetric;
+
+        auto encoding = CBLVectorEncoding_CreateNone;
+
+        try
+        {
+            bits = <uint32_t>(body["bits"]);
+            subquantizers = <uint32_t>(body["subquantizers"]);
+        }
+        catch (...)
+        {
+            bits.reset();
+            subquantizers.reset();
+        }
+
+        try
+        {
+            scalarEncoding = <CBLScalarQuantizerType>(body["scalarEncoding"]);
+        }
+        catch (...)
+        {
+            scalarEncoding.reset();
+        }
+
+        if (scalarEncoding.has_value() && (bits.has_value() || subquantizers.has_value()))
+        {
+            throw std::invalid_argument("Cannot have scalar quantization and arguments for product quantization at the same time");
+        }
+
+        if ((bits.has_value() && !subquantizers.has_value()) || (!bits.has_value() && subquantizers.has_value()))
+        {
+            throw std::invalid_argument("Product quantization requires both bits and subquantizers set");
+        }
+
+        if (scalarEncoding.has_value())
+        {
+            encoding = CBLVectorEncoding_CreateScalarQuantizer(scalarEncoding.value());
+        }
+        if (bits.has_value() && subquantizers.has_value())
+        {
+            encoding = CBLVectorEncoding_CreateProductQuantizer(subquantizers.value(), bits.value());
+        }
+
+        if (metric == "euclidean")
+        {
+            dMetric = kCBLDistanceMetricEuclidean;
+        }
+        else if (metric == "cosine")
+        {
+            dMetric = kCBLDistanceMetricCosine;
+        }
+        else
+        {
+            throw std::invalid_argument("Invalid distance metric");
+        }
+
         with<CBLDatabase *>(body, "database", [conn](CBLDatabase* db)
                             {
-                                auto scopeName = body["scopeName"].get<string>();
-                                auto collectionName = body["collectionName"].get<string>();
-                                auto indexName = body["indexName"].get<string>();
-                                auto expression = body["expression"].get<string>();
-                                auto metric = body["metric"].get<string>();
-
-                                auto dimensions = body["dimensions"].get<uint32_t>();
-                                auto centroids = body["centroids"].get<uint32_t>();
-                                auto minTrainingSize = body["minTrainingSize"].get<uint32_t>();
-                                auto maxTrainingSize = body["maxTrainingSize"].get<uint32_t>();
-
-                                std::optional<uint32_t> bits;
-                                std::optional<uint32_t> subquantizers;
-                                std::optional<CBLScalarQuantizerType> scalarEncoding;
-                                std::optional<CBLDistanceMetric> dMetric;
-
-                                auto encoding = CBLVectorEncoding_CreateNone;
-
-                                try
-                                {
-                                    bits = <uint32_t>(body["bits"]);
-                                    subquantizers = <uint32_t>(body["subquantizers"]);
-                                }
-                                catch (...)
-                                {
-                                    bits.reset();
-                                    subquantizers.reset();
-                                }
-
-                                try
-                                {
-                                    scalarEncoding = <CBLScalarQuantizerType>(body["scalarEncoding"]);
-                                }
-                                catch (...)
-                                {
-                                    scalarEncoding.reset();
-                                }
-
-                                if (scalarEncoding.has_value() && (bits.has_value() || subquantizers.has_value()))
-                                {
-                                    throw std::invalid_argument("Cannot have scalar quantization and arguments for product quantization at the same time");
-                                }
-
-                                if ((bits.has_value() && !subquantizers.has_value()) || (!bits.has_value() && subquantizers.has_value()))
-                                {
-                                    throw std::invalid_argument("Product quantization requires both bits and subquantizers set");
-                                }
-
-                                if (scalarEncoding.has_value())
-                                {
-                                    encoding = CBLVectorEncoding_CreateScalarQuantizer(scalarEncoding.value());
-                                }
-                                if (bits.has_value() && subquantizers.has_value())
-                                {
-                                    encoding = CBLVectorEncoding_CreateProductQuantizer(subquantizers.value(), bits.value());
-                                }
-
-                                if (metric == "euclidean")
-                                {
-                                    dMetric = kCBLDistanceMetricEuclidean;
-                                }
-                                else if (metric == "cosine")
-                                {
-                                    dMetric = kCBLDistanceMetricCosine;
-                                }
-                                else
-                                {
-                                    throw std::invalid_argument("Invalid distance metric");
-                                }
 
                                 CBLVectorIndexConfiguration config{kCBLN1QLLanguage, expression, dimensions, centroids};
                                 config.encoding = encoding;
