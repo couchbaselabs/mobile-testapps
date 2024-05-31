69591a9e0696dabf803fe60befa2c6172f2012a2
Gilad Kalchheim <gilkal1979@gmail.com>
2024-05-31 11:11:09 +0100

    Use with only when creating the collection

--- CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
+++ CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
@@ -17,102 +17,107 @@ namespace vectorSearch_methods
 
     void vectorSearch_createIndex(json& body, mg_connection* conn)
     {
-        auto scopeName = body["scopeName"].get<string>();
-        auto collectionName = body["collectionName"].get<string>();
-        auto indexName = body["indexName"].get<string>();
-        auto expression = body["expression"].get<string>();
-        auto metric = body["metric"].get<string>();
+            auto scopeName = body["scopeName"].get<string>();
+            auto collectionName = body["collectionName"].get<string>();
+            auto indexName = body["indexName"].get<string>();
+            auto expression = body["expression"].get<string>();
+            auto metric = body["metric"].get<string>();
 
-        auto dimensions = body["dimensions"].get<uint32_t>();
-        auto centroids = body["centroids"].get<uint32_t>();
-        auto minTrainingSize = body["minTrainingSize"].get<uint32_t>();
-        auto maxTrainingSize = body["maxTrainingSize"].get<uint32_t>();
+            auto dimensions = body["dimensions"].get<uint32_t>();
+            auto centroids = body["centroids"].get<uint32_t>();
+            auto minTrainingSize = body["minTrainingSize"].get<uint32_t>();
+            auto maxTrainingSize = body["maxTrainingSize"].get<uint32_t>();
 
-        std::optional<uint32_t> bits;
-        std::optional<uint32_t> subquantizers;
-        std::optional<CBLScalarQuantizerType> scalarEncoding;
-        std::optional<CBLDistanceMetric> dMetric;
+            std::optional<uint32_t> bits;
+            std::optional<uint32_t> subquantizers;
+            std::optional<CBLScalarQuantizerType> scalarEncoding;
+            std::optional<CBLDistanceMetric> dMetric;
 
-        auto* encoding = CBLVectorEncoding_CreateNone();
+            auto* encoding = CBLVectorEncoding_CreateNone();
+            CBLCollection* collection;
 
-        try
-        {
-            bits = static_cast<uint32_t>(body["bits"]);
-            subquantizers = static_cast<uint32_t>(body["subquantizers"]);
-        }
-        catch (...)
-        {
-            bits.reset();
-            subquantizers.reset();
-        }
+            with<CBLDatabase *>(body,"database", [conn, &collectionName, &scopeName](CBLDatabase* db)
+            {
+                CBLError err = {};
+                collection = CBLDatabase_CreateCollection(db, flstr(collectionName),  flstr(scopeName), &err);
+                if(err.code!=0) {
+                    write_serialized_body(conn, CBLError_Message(&err));
+                }
+            });
 
-        try
-        {
-            scalarEncoding = static_cast<CBLScalarQuantizerType>(body["scalarEncoding"]);
-        }
-        catch (...)
-        {
-            scalarEncoding.reset();
-        }
+            try
+            {
+                bits = static_cast<uint32_t>(body["bits"]);
+                subquantizers = static_cast<uint32_t>(body["subquantizers"]);
+            }
+            catch (...)
+            {
+                bits.reset();
+                subquantizers.reset();
+            }
 
-        if (scalarEncoding.has_value() && (bits.has_value() || subquantizers.has_value()))
-        {
-            throw std::invalid_argument("Cannot have scalar quantization and arguments for product quantization at the same time");
-        }
+            try
+            {
+                scalarEncoding = static_cast<CBLScalarQuantizerType>(body["scalarEncoding"]);
+            }
+            catch (...)
+            {
+                scalarEncoding.reset();
+            }
 
-        if ((bits.has_value() && !subquantizers.has_value()) || (!bits.has_value() && subquantizers.has_value()))
-        {
-            throw std::invalid_argument("Product quantization requires both bits and subquantizers set");
-        }
+            if (scalarEncoding.has_value() && (bits.has_value() || subquantizers.has_value()))
+            {
+                throw std::invalid_argument("Cannot have scalar quantization and arguments for product quantization at the same time");
+            }
 
-        if (scalarEncoding.has_value())
-        {
-            encoding = static_cast<CBLVectorEncoding*>(CBLVectorEncoding_CreateScalarQuantizer(scalarEncoding.value()));
-        }
-        if (bits.has_value() && subquantizers.has_value())
-        {
-            encoding = static_cast<CBLVectorEncoding*>(CBLVectorEncoding_CreateProductQuantizer(subquantizers.value(), bits.value()));
-        }
+            if ((bits.has_value() && !subquantizers.has_value()) || (!bits.has_value() && subquantizers.has_value()))
+            {
+                throw std::invalid_argument("Product quantization requires both bits and subquantizers set");
+            }
 
-        if (metric == "euclidean")
-        {
-            dMetric = kCBLDistanceMetricEuclidean;
-        }
-        else if (metric == "cosine")
-        {
-            dMetric = kCBLDistanceMetricCosine;
-        }
-        else
-        {
-            throw std::invalid_argument("Invalid distance metric");
-        }
+            if (scalarEncoding.has_value())
+            {
+                encoding = static_cast<CBLVectorEncoding*>(CBLVectorEncoding_CreateScalarQuantizer(scalarEncoding.value()));
+            }
+            if (bits.has_value() && subquantizers.has_value())
+            {
+                encoding = static_cast<CBLVectorEncoding*>(CBLVectorEncoding_CreateProductQuantizer(subquantizers.value(), bits.value()));
+            }
 
-        with<CBLDatabase *>(body, "database", [conn](CBLDatabase* db)
-                            {
+            if (metric == "euclidean")
+            {
+                dMetric = kCBLDistanceMetricEuclidean;
+            }
+            else if (metric == "cosine")
+            {
+                dMetric = kCBLDistanceMetricCosine;
+            }
+            else
+            {
+                throw std::invalid_argument("Invalid distance metric");
+            }
 
-                               // CBLVectorIndexConfiguration config{kCBLN1QLLanguage, expression, dimensions, centroids};
-                                CBLVectorIndexConfiguration config = {};
-                                config.expression = expression;
-                                config.dimensions = dimensions;
-                                config.encoding = encoding;
-                                config.metric = metric;
-                                config.centroids = centroids;
-                                config.minTrainingSize = minTrainingSize;
-                                config.maxTrainingSize = maxTrainingSize;
 
-                                CBLError err = {};
-                                auto collection = CBLDatabase_Collection(db, collectionName, scopeName, &err);
+            // CBLVectorIndexConfiguration config{kCBLN1QLLanguage, expression, dimensions, centroids};
+            CBLVectorIndexConfiguration config = {};
+            config.expression = expression;
+            config.dimensions = dimensions;
+            config.encoding = encoding;
+            config.metric = metric;
+            config.centroids = centroids;
+            config.minTrainingSize = minTrainingSize;
+            config.maxTrainingSize = maxTrainingSize;
 
-                                try
-                                {
-                                    CBLCollection_CreateVectorIndex(collection, indexName, config);
-                                    std::cout << "Successfully created index" << std::endl;
-                                    write_serialized_body(conn, "Created index with name " + indexName);
-                                }
-                                catch (const std::exception &e)
-                                {
-                                    std::cout << "Failed to create index" << std::endl;
-                                    write_serialized_body(conn, "Could not create index: " + e.what());
-                                } });
+            try
+            {
+                CBLCollection_CreateVectorIndex(collection, indexName, config);
+                std::cout << "Successfully created index" << std::endl;
+                write_serialized_body(conn, "Created index with name " + indexName);
+            }
+            catch (const std::exception &e)
+            {
+                std::cout << "Failed to create index" << std::endl;
+                write_serialized_body(conn, "Could not create index: " + e.what());
+            }    
     }
 }
