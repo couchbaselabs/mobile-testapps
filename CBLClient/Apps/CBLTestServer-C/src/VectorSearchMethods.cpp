16b24c7bd630b527a71ca145139f5e09a3c6ecd5
Gilad Kalchheim <gilkal1979@gmail.com>
2024-06-01 21:06:51 +0100

    debug createIndex

--- CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
+++ CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
@@ -145,13 +145,12 @@ namespace vectorSearch_methods
         TRY(db = CBLDatabase_Open(flstr(dbName), databaseConfig, &err), err);
          write_serialized_body(conn, memory_map::store(db, CBLDatabase_EntryDelete));
     }
-    void vectorSearch_resgisterModel(json& body, mg_connection* conn) {
+   /* void vectorSearch_resgisterModel(json& body, mg_connection* conn) {
         const auto name = body["name"].get<string>();
         const auto key = body["key"].get<string>();
         CBLPredictiveModel model;
-        model.
         TRY(CBL_RegisterPredictiveModel())
-    }
+    }*/
 
     /* public sealed class VectorModel : IPredictiveModel
         {
