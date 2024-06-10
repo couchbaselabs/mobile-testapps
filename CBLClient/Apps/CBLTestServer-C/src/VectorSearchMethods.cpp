7b0684ba865c6a45dacf928bf91a699fad90e8f8
Gilad Kalchheim <gilkal1979@gmail.com>
2024-06-10 07:15:28 +0100

    Set the embeddings as a vector

--- CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
+++ CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
@@ -40,8 +40,7 @@ FLSliceResult predictFunction(void* context, FLDict input) {
     auto embbedingsVector =  FLMutableArray_New();
     const FLValue inputWord = FLDict_Get(input, flstr("word"));
     if (inputWord) {
-        const FLValue wordEmbeddings = FLDict_Get(wordMap, FLValue_AsString(inputWord));
-        embbedingsVector = FLMutableArray_SetArray(FLValue_AsArray(wordEmbeddings));
+        embbedingsVector = FLMutableArray_SetArray(FLValue_AsArray(FLDict_Get(wordMap, FLValue_AsString(inputWord))));
     }
     FLEncoder enc = FLEncoder_New();
     if (embbedingsVector) {
