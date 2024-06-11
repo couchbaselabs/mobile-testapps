c80980aff65b41c90e21fb686e02ce49763a35cd
Gilad Kalchheim <gilkal1979@gmail.com>
2024-06-11 12:27:56 +0100

    Attemp to refactor

--- CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
+++ CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
@@ -40,17 +40,15 @@ FLSliceResult predictFunction(void* context, FLDict input) {
     auto embbedingsVector =  FLMutableArray_New();
     const FLValue inputWord = FLDict_Get(input, flstr("word"));
     if (inputWord) {
-        const FLValue tempVector = FLDict_Get(wordMap, FLValue_AsString(inputWord));
-        //FLMutableArray_SetArray(tempMutableArray, FLValue_AsArray(tempVector));
-       // appendLogMessage("For word: " + to_string(FLValue_AsString(inputWord)) + " ");
+        /*const FLValue tempVector = FLDict_Get(wordMap, FLValue_AsString(inputWord));
         FLArrayIterator iter;
         FLArrayIterator_Begin(FLValue_AsArray(tempVector), &iter);
         FLValue value;
         while (NULL != (value = FLArrayIterator_GetValue(&iter))) {
             FLMutableArray_AppendFloat(embbedingsVector, FLValue_AsFloat(value));
             FLArrayIterator_Next(&iter);
-        }
-        //FLMutableArray_AppendArray(embbedingsVector, FLValue_AsArray(FLDict_Get(wordMap, FLValue_AsString(inputWord))));
+        }*/
+        FLMutableArray_AppendArray(embbedingsVector, FLValue_AsArray(FLDict_Get(wordMap, FLValue_AsString(inputWord))));
     }
     FLEncoder enc = FLEncoder_New();
     if (embbedingsVector) {
