3c1bea77a943a62b24af7af90fe91675018df0c2
Gilad Kalchheim <gilkal1979@gmail.com>
2024-06-12 17:34:25 +0100

    Debug refactoring problem

--- CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
+++ CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
@@ -170,7 +170,7 @@ static FLMutableDict getWordMap() {
             }
          }
          CBLQuery_Release(query2);*/
-         return FLValue_AsDict(FLDict_Get(words, flstr("results")));
+         return FLDict_AsMutable(FLValue_AsDict(FLDict_Get(words, flstr("results"))));
 }
 
 FLDict getEmbeddingDic(string term) {
