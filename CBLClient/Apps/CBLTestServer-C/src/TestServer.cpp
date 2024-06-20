71d972b5f72783236b260d44ffb6be6c8b039a72
Gilad Kalchheim <gilkal1979@gmail.com>
2024-06-20 08:11:49 +0100

    Switch the smlink paramters

--- CBLClient/Apps/CBLTestServer-C/src/TestServer.cpp
+++ CBLClient/Apps/CBLTestServer-C/src/TestServer.cpp
@@ -31,7 +31,7 @@ static int handle_request(mg_connection* connection, void* context) {
 void TestServer::start() {
     string port_str = to_string(PORT);
     const char* options[3] = {"listening_ports", port_str.c_str(), nullptr};
-    symlink("/root/ctestserver/Extensions/libgomp.so.1", "/root/ctestserver/Extensions/libgomp.so.1.0.0");
+    symlink("/root/ctestserver/Extensions/libgomp.so.1.0.0", "/root/ctestserver/Extensions/libgomp.so.1");
     memory_map::init();
     _httpContext = mg_start(nullptr, nullptr, options);
     mg_set_request_handler(_httpContext, "/*", handle_request, nullptr);
