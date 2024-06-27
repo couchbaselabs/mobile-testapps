19e90205b7fcc4d0c0e4a0524c38588e8879ab00
Gilad Kalchheim <gilkal1979@gmail.com>
2024-06-27 14:32:35 +0100

    Add a conversion to flstr

--- CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
+++ CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
@@ -30,26 +30,28 @@ FLMutableDict getPrediction(FLDict input, string key) {
 }
 
 FLMutableDict predictFunction(void* context, FLDict input) {
-    auto embbedingsVector =  FLMutableArray_New();
+    //auto embbedingsVector =  FLMutableArray_New();
     auto predictDict = FLMutableDict_New();
     DEFER {
-        FLMutableArray_Release(embbedingsVector);
+      //  FLMutableArray_Release(embbedingsVector);
         FLMutableDict_Release(predictDict);
     };
     const FLValue inputWord = FLDict_Get(input, flstr("word"));
     if (inputWord) {
-        const FLValue tempVector = FLDict_Get(wordMap, FLValue_AsString(inputWord));
-        FLArrayIterator iter;
+        const FLValue embbedingsVector = FLDict_Get(wordMap, FLValue_AsString(inputWord));
+       /* FLArrayIterator iter;
         FLArrayIterator_Begin(FLValue_AsArray(tempVector), &iter);
         FLValue value;
         while (NULL != (value = FLArrayIterator_GetValue(&iter))) {
             FLMutableArray_AppendFloat(embbedingsVector, FLValue_AsFloat(value));
             FLArrayIterator_Next(&iter);
         }
-    }
-    if (embbedingsVector) {
+    }*/
         FLMutableDict_SetArray(predictDict, flstr("vector"), FLValue_AsArray(embbedingsVector));
     }
+    //if (embbedingsVector) {
+    //    FLMutableDict_SetArray(predictDict, flstr("vector"), FLValue_AsArray(embbedingsVector));
+    //}
     /*FLEncoder enc = FLEncoder_New();
     if (embbedingsVector) {
         FLEncoder_BeginDict(enc, 1);
@@ -57,7 +59,8 @@ FLMutableDict predictFunction(void* context, FLDict input) {
         FLEncoder_WriteValue(enc, FLValue(embbedingsVector));
         FLEncoder_EndDict(enc);
     }*/
-    return FLEncoder_Finish(enc, nullptr); 
+    //return FLEncoder_Finish(enc, nullptr); 
+    return predictDict;
 }
 
 static void CBLDatabase_EntryDelete(void* ptr) {
