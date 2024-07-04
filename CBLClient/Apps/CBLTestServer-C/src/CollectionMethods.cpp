c4c18aeebc1aefba44018a7959d14f530d2f569a
Gilad Kalchheim <gilkal1979@gmail.com>
2024-07-04 15:17:19 +0100

    Add a collection getIndex function

--- CBLClient/Apps/CBLTestServer-C/src/CollectionMethods.cpp
+++ CBLClient/Apps/CBLTestServer-C/src/CollectionMethods.cpp
@@ -157,7 +157,7 @@ namespace collection_methods {
     void collection_getIndex(json& body, mg_connection* conn) {
         auto indexName = body["indexName"].get<string>();
         with<CBLCollection *>(body,"collection",[conn, indexName](CBLCollection* collection) {
-            write_serialized_body(conn, memory_map::store(CBLCollection_GetIndex(collection, indexName)));
+            write_serialized_body(conn, memory_map::store(CBLCollection_GetIndex(collection, flstr(indexName))));
         });
     }
 
