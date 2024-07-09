49ff7bb52075d94cb80bb9187275a3f86af24771
Gilad Kalchheim <gilkal1979@gmail.com>
2024-07-09 06:40:53 +0100

    Remove debug prints

--- CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
+++ CBLClient/Apps/CBLTestServer-C/src/VectorSearchMethods.cpp
@@ -333,7 +333,7 @@ namespace vectorSearch_methods
         TRY(updater = CBLQueryIndex_BeginUpdate(index, documentUpdateLimit, &err), err);
         for (int i=0; i<CBLIndexUpdater_Count(updater); i++) {
             const FLArray embeddingVector = FLValue_AsArray(FLDict_Get(wordMap, FLValue_AsString(CBLIndexUpdater_Value(updater, i))));
-            outfile.open("test.txt", std::ios_base::app);
+            MyFile.open("test.txt", std::ios_base::app);
             MyFile << to_string(FLValue_AsString(CBLIndexUpdater_Value(updater, i))) + "\n";
             MyFile.close();
             if (embeddingVector) {
