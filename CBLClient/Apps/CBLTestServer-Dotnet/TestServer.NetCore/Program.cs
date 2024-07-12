7aa886bd2d4af23b0fc550d8f7d5e390f9d0bf8e
Gilad Kalchheim <gilkal1979@gmail.com>
2024-07-12 08:33:37 +0100

    Fix a typo

--- CBLClient/Apps/CBLTestServer-Dotnet/TestServer.NetCore/Program.cs
+++ CBLClient/Apps/CBLTestServer-Dotnet/TestServer.NetCore/Program.cs
@@ -51,7 +51,7 @@ namespace Couchbase.Lite.Testing.NetCore
 
         static void Main(string[] args)
         {
-            Extension.Load(new VectorSearchExtension());
+            Extensions.Load(new VectorSearchExtension());
             Extend();
 
             Database.Log.Console.Level = Logging.LogLevel.None;
