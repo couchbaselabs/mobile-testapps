2c526590a84efcace4727ea51523116c8c973962
Gilad Kalchheim <gilkal1979@gmail.com>
2024-07-30 10:22:41 +0100

    Debug

--- CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandlerDispatcher.java
+++ CBLClient/Apps/JavaRequestHandlers/src/main/java/com/couchbase/mobiletestkit/javacommon/RequestHandlerDispatcher.java
@@ -136,7 +136,7 @@ public class RequestHandlerDispatcher {
                         requestHandler = new VectorSearchRequestHandler();
                         break;
                     default:
-                        throw new IllegalArgumentException("Handler not implemented for this call " + handlerType);
+                        throw new IllegalArgumentException("Gilad");
                 }
                 if (target.getReturnType().equals(Void.TYPE)) {
                     target.invoke(requestHandler, args);
