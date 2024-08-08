b8ede858bb925a87a50f122cbaf2fb3043601c0c
Gilad Kalchheim <gilkal1979@gmail.com>
2024-08-08 13:37:58 +0100

    Attempt to disalbe vector search

--- CBLClient/Apps/CBLTestServer-Android/app/src/main/java/com/couchbase/CouchbaseLiteServ/CouchbaseLiteServ.java
+++ CBLClient/Apps/CBLTestServer-Android/app/src/main/java/com/couchbase/CouchbaseLiteServ/CouchbaseLiteServ.java
@@ -48,11 +48,11 @@ public class CouchbaseLiteServ extends Application {
 
         Log.init(new TestServerLogger());
         CouchbaseLite.init(this, true);
-        try {
-            CouchbaseLite.enableVectorSearch();
-        }
-        catch (Exception e) {
-            Log.i(TAG, "Warning: vector search was not loaded, the vector serach tests are expected to fail");
-        }
+        //try {
+        //    CouchbaseLite.enableVectorSearch();
+       // }
+        //catch (Exception e) {
+        //    Log.i(TAG, "Warning: vector search was not loaded, the vector serach tests are expected to fail");
+        //}
     }
 }
